db1af4198ea53cb93cd2250637bd63e04ae26cc9
imxiaoxiaohh <ceciliafu2000@gmail.com>
2024-03-19 21:37:16 +0100

    done for disc03

---
+++ lab02/disc03.py
@@ -0,0 +1,70 @@
+def swipe(n):
+    """Print the digits of n, one per line, first backward then forward.
+
+    >>> swipe(2837)
+    7
+    3
+    8
+    2
+    8
+    3
+    7
+    """
+    if n < 10:
+        print(n)
+    else:
+        "*** YOUR CODE HERE ***"
+        print ( n%10 )
+        swipe ( n//10 )
+        print ( n%10)
+
+swipe(2817)
+
+def skip_factorial(n):
+    """Return the product of positive integers n * (n - 2) * (n - 4) * ...
+
+    >>> skip_factorial(5) # 5 * 3 * 1
+    15
+    >>> skip_factorial(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n <= 2:
+        return n
+    else:
+        return n * skip_factorial(n - 2)
+skip_factorial(5)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, 
+    and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+def even(n):
+    return hailstone(n/2)
+
+def odd(n):
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return n
+    else:
+        hailstone(3*n + 1)
+hailstone(10)
